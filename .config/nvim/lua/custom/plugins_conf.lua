91c4789c1b4839f50a031104a15bbc228675090d
Vladislav Lebedev <tonknaf@gmail.com>
2023-05-16 14:35:27 +0300

    add lsps

--- .config/nvim/lua/custom/plugins_conf.lua
+++ .config/nvim/lua/custom/plugins_conf.lua
@@ -178,12 +178,16 @@ end
 --  Add any additional override configuration in the following tables. They will be passed to
 --  the `settings` field of the server config. You must look up that documentation yourself.
 local servers = {
-  -- clangd = {},
+  clangd = {},
   gopls = {},
   phpactor = {},
-  -- pyright = {},
+  pyright = {},
   -- rust_analyzer = {},
   tsserver = {},
+  cssls = {},
+  html = {},
+  jsonls = {},
+  graphql = {},
 
   lua_ls = {
     Lua = {
