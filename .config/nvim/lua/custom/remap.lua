cb62b6cebf6f7018d07aca4bfc87a93697455440
Vladislav Lebedev <tonknaf@gmail.com>
2023-05-21 13:42:49 +0300

    improve term

--- .config/nvim/lua/custom/remap.lua
+++ .config/nvim/lua/custom/remap.lua
@@ -107,6 +107,37 @@ vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = "Open float
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
 
 -- Split line
-vim.api.nvim_set_keymap('n', '<leader>s', ':normal! i<CR><Esc>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<A-n>', 'i<CR><Esc>', { noremap = true, silent = true })
 
-vim.api.nvim_set_keymap('n', '<leader>tt', '<CMD>ToggleTerm<CR>', {noremap = true, silent = true})
+
+function _G.set_terminal_keymaps()
+  local opts = { buffer = 0 }
+  vim.keymap.set('t', '<A-v>', [[<C-\><C-n>]], opts)
+  vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+  vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+  vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+  vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+  vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
+end
+
+-- if you only want these mappings for toggle term use term://*toggleterm#* instead
+vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
+vim.api.nvim_set_keymap('n', '<leader>tt', '<CMD>ToggleTerm<CR>', { noremap = true, silent = true })
+
+local Terminal = require('toggleterm.terminal').Terminal
+local gitui    = Terminal:new({
+  cmd = "gitui",
+  hidden = true,
+  direction = "float",
+  dir = "git_dir",
+  on_open = function(term)
+    vim.cmd("startinsert!")
+    vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", { noremap = true, silent = true })
+  end,
+})
+
+function _gitui_toggle()
+  gitui:toggle()
+end
+
+vim.api.nvim_set_keymap("n", "<leader>tg", "<cmd>lua _gitui_toggle()<CR>", { noremap = true, silent = true })
